7f212091f27a8eaafb9d79542241c8191b77057c
Daniel Brooks <db48x@db48x.net>
2010-12-09 03:49:46 -0600

    actually do the conversion and get them both working; the previous commit merely contained a rename

--- plugins/playlist/popcorn.playanother.js
+++ plugins/playlist/popcorn.playanother.js
@@ -1,44 +1,42 @@
-(function (Popcorn) {
-  Popcorn.plugin("playanother", (function()
+(function (Popcorn)
+{
+  Popcorn.plugin("playanother",
   {
-    var target, isIn;
+    _setup: function(options)
+    {
+      var target = $(options.type == "audio" ? "<audio/>" : "<video/>",
+                     { src: options.src,
+                       width: options.width,
+                       height: options.height,
+                       id: this.id,
+                       preload: 'auto'
+                     });
 
-    return {
-      _setup: function(event, options)
-      {
-        target = $(options.type == "audio" ? "<audio/>" : "<video/>",
-                   { src: options.src,
-                     width: options.width,
-                     height: options.height,
-                     id: this.id,
-                     preload: 'auto'
-                   });
+      options._target = target;
+      options._isIn = false;
+      target.hide();
+      $("#"+ options.target).append(target);
 
-        target.hide();
-        $("#"+ options.target).append(target);
-
-        var self = this;
-        var vid = $(this.video);
-        vid.bind("play", function() { if (self.isIn) target.get(0).play(); })
-           .bind("pause", function() { target.get(0).pause(); })
-           .bind("volumechange", function(event)
-        {
-          target.get(0).volume = vid.volume;
-          target.get(0).muted = vid.muted;
-        });
-      },
-      start: function(event, options)
+      var vid = $(this.video);
+      vid.bind("play", function() { if (options._isIn) target.get(0).play(); })
+         .bind("pause", function() { target.get(0).pause(); })
+         .bind("volumechange", function(event)
       {
-        target.show();
-        target.get(0).play();
-        this.isIn = true;
-      },
-      end: function(event, options)
-      {
-        target.get(0).pause();
-        target.hide();
-        this.isIn = false;
-      },
-    };
-  }));
+        target.get(0).volume = vid.volume;
+        target.get(0).muted = vid.muted;
+      });
+    },
+    start: function(event, options)
+    {
+      options._target.show();
+      options._target.get(0).play();
+      options._isIn = true;
+    },
+    end: function(event, options)
+    {
+      options._target.get(0).pause();
+      options._target.hide();
+      options._isIn = false;
+    }
+  });
 })(Popcorn);
--- plugins/playlist/popcorn.playlist.js
+++ plugins/playlist/popcorn.playlist.js
@@ -1,50 +1,43 @@
 (function (Popcorn)
 {
-  Popcorn.plugin("playlist" , (function()
+  Popcorn.plugin("playlist",
   {
-    var target, tracks, len, current;
-    var isIn = false;
+    _setup: function(options)
+    {
+      var target = options._target = $("#"+ options.target),
+          tracks = options._tracks = target.children("audio, video"),
+          current = options._current = tracks[0];
+      options._isIn = false;
 
-    return {
-      _setup: function(event, options)
+      var len = tracks.length;
+      tracks.each(function(i)
       {
-        alert('_setup');
-        target = $("#"+ options.target);
-        tracks = target.children("audio, video");
-        len = tracks.length;
-        current = tracks[0];
+        $(this).bind("ended", function() { (current = tracks[(i == len) ? 0 : i+1]).play(); });
+      });
 
-        tracks.each(function(i)
+      var self = this;
+      var vid = $(this.video);
+      vid.bind("play", function() { if (self.isIn) current.play(); })
+         .bind("pause", function() { tracks.each(function() { this.pause(); }); })
+         .bind("ended", function() { tracks.each(function() { this.pause(); }); })
+         .bind("volumechange", function()
+      {
+        music.each(function()
         {
-          $(this).bind("ended", function() { (current = tracks[(i == len) ? 0 : i+1]).play(); });
+          this.volume = vid.volume;
+          this.muted = vid.muted;
         });
-
-        var self = this;
-        var vid = $(this.video);
-        vid.bind("play", function() { if (self.isIn) current.play(); })
-           .bind("pause", function() { tracks.each(function() { this.pause(); }); })
-           .bind("ended", function() { tracks.each(function() { this.pause(); }); })
-           .bind("volumechange", function()
-        {
-          music.each(function()
-          {
-            this.volume = vid.volume;
-            this.muted = vid.muted;
-          });
-        });
-      },
-      start: function(event, options)
-      {
-        alert('start');
-        current.play();
-        this.isIn = true;
-      },
-      end: function(event, options)
-      {
-        alert('end');
-        tracks.each(function() { this.pause(); });
-        this.isIn = false;
-     },
-    };
-  }));
+      });
+    },
+    start: function(event, options)
+    {
+      options._current.play();
+      options._isIn = true;
+    },
+    end: function(event, options)
+    {
+      options._tracks.each(function() { this.pause(); });
+      options._isIn = false;
+   },
+  });
 })(Popcorn);

ad04de8af95eedb42467a550030d91ba5795ff65
Daniel Brooks <db48x@db48x.net>
2010-12-20 12:08:35 -0600

    [#176] add a simple test for the DOMContentLoaded feature

--- test/popcorn.unit.js
+++ test/popcorn.unit.js
@@ -2,7 +2,7 @@
 module("Popcorn");
 test("API", function () {
   
-  var expects = 3, 
+  var expects = 4, 
       count = 0;
   
   expect(expects);
@@ -34,6 +34,12 @@ test("API", function () {
     
   } catch (e) {};
 
+  try {  
+    
+    Popcorn(function() { ok(1, "Popcorn calls its function argument"); });
+    plus();
+    
+  } catch (e) {};
   
 });
 
